67e0163780b2d2e77c0762df26292a72aaf3763c
sjcano <sjcano@users.noreply.github.com>
2016-04-28 13:31:47 +0200

    Update views.py

--- welcome/views.py
+++ welcome/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
 from sqlalchemy import *
-from project import database
+from project import database as pdb
 
 from . import database
 from .models import PageView
@@ -15,7 +15,7 @@ def index(request):
     PageView.objects.create(hostname=hostname)
 
     # check database connectivity
-    db = create_engine(database.url())
+    db = create_engine(pdb.url())
     connection = engine.connect()
     result = connection.execute("select 1")
     connection.close()

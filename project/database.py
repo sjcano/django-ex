4f30536122395ca7d0386214ccddf2df7b280d21
sjcano <sjcano@users.noreply.github.com>
2016-04-28 11:26:34 +0200

    Update database.py

--- project/database.py
+++ project/database.py
@@ -10,6 +10,17 @@ engines = {
 }
 
 
+def url():
+    engine = os.getenv('DATABASE_ENGINE')
+    host = os.getenv('{}_SERVICE_HOST'.format(service_name))
+    port = os.getenv('{}_SERVICE_PORT'.format(service_name))
+    user = os.getenv('DATABASE_USER')
+    password = os.getenv('DATABASE_PASSWORD')
+    name = os.getenv('DATABASE_NAME')
+    # engine://user:password@host:port/name
+    url = "%s://%s:%s@%s:%s/%s" % (engine, user, password, host, port, name)
+    return url
+    
 def config():
     service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper()
     if service_name:
